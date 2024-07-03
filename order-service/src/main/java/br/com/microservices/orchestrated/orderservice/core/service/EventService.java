6ced77f10673dc81715ed2467ffe8d0a38814d47
anderson <abaeta@gmail.com>
2024-07-03 19:00:52 -0300

    Correção de bugs no order-service

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
@@ -6,11 +6,12 @@ import br.com.microservices.orchestrated.orderservice.core.dto.EventFilters;
 import br.com.microservices.orchestrated.orderservice.core.repository.EventRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+
+import static org.springframework.util.ObjectUtils.isEmpty;
 
 @Slf4j
 @Service
@@ -20,7 +21,7 @@ public class EventService {
     private final EventRepository repository;
 
     public void notifyEnding(Event event) {
-        event.setOrderId(event.getOrderId());
+        event.setOrderId(event.getPayload().getId());
         event.setCreatedAt(LocalDateTime.now());
         save(event);
         log.info("Order {} with Saga notified! TransactionID: {}", event.getOrderId(), event.getTransactionId());
@@ -32,7 +33,7 @@ public class EventService {
 
     public Event findByFilters(EventFilters filters){
         validateEmptyFilters(filters);
-        if (!ObjectUtils.isEmpty(filters.getOrderId())){
+        if (isEmpty(filters.getOrderId())){
             return findByOrderId(filters.getOrderId());
         } else {
             return findByTransactionId(filters.getTransactionId());
@@ -46,11 +47,11 @@ public class EventService {
 
     private Event findByTransactionId(String transactionId){
         return repository.findTop1ByTransactionIdOrderByCreatedAtDesc(transactionId)
-                .orElseThrow(() -> new ValidationException("Event not found by transactionID."));
+                .orElseThrow(() -> new ValidationException("Event not found by transactionId."));
     }
 
     private void validateEmptyFilters(EventFilters filters){
-        if(ObjectUtils.isEmpty(filters.getOrderId()) && ObjectUtils.isEmpty(filters.getTransactionId())){
+        if(isEmpty(filters.getOrderId()) && isEmpty(filters.getTransactionId())){
             throw new ValidationException("OrderID of TransactionID must be informed.");
         }
     }
--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/OrderService.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/OrderService.java
@@ -7,12 +7,14 @@ import br.com.microservices.orchestrated.orderservice.core.producer.SagaProducer
 import br.com.microservices.orchestrated.orderservice.core.repository.OrderRepository;
 import br.com.microservices.orchestrated.orderservice.core.utils.JsonUtil;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class OrderService {
